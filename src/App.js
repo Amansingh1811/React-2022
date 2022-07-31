efb9a425bac4360b496c81ec5e8ee618d7f2864c
Amansingh1811 <amansingh18112000@gmai.com>
2022-07-31 10:10:34 +0530

    add some notes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 // import UseEffect from './components/hooks/useEffect';
 // import Reastaurant from './components/basics/ Restaurant'
 // import UseState from './components/hooks/useState';
-import UseReducer from './components/hooks/useReducer';
+// import UseReducer from './components/hooks/useReducer';
+import Todoitem from './components/todoitem/todoitem';
+
 
 
 const App = () => {
-  return <UseReducer/>
-  
+  return <Todoitem/>
 }
 export default App;
